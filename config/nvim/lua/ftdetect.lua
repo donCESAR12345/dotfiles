e8b8ba643bfd85764812cff75087e9d01f65f0cc
donCESAR12345 <sprit152009@gmail.com>
2026-05-09 15:31:20 -0500

    chore: remove legacy jupyter and distant.nvim experiments

--- config/nvim/lua/ftdetect.lua
+++ config/nvim/lua/ftdetect.lua
@@ -3,8 +3,6 @@ local M = {}
 M.setup = function()
 	vim.filetype.add({
 		extension = {
-			-- Jupyter notebooks
-			ipynb = "ipynb",
 			-- Terraform variable files (required for terraformls)
 			tfvars = "terraform-vars",
 		},
