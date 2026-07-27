3b973240e6a475135f97c701b781bacab5cf4f2c
donCESAR12345 <sprit152009@gmail.com>
2026-07-26 21:49:47 -0500

    feat(nvim): add sqls & yamlls LSPs, and sql-formatter & yamlfmt formatting

--- config/nvim/lua/plugins/lsp-config.lua
+++ config/nvim/lua/plugins/lsp-config.lua
@@ -48,8 +48,10 @@ local plugins = {
 				"pyright", -- Python (type checking)
 				"ruff", -- Python (linting + formatting)
 				"lua_ls", -- Lua
+				"sqls", -- SQL
 				"terraformls", -- Terraform
 				"ts_ls", -- TypeScript / JavaScript
+				"yamlls", -- YAML
 			}
 
 			for _, server in ipairs(servers) do
@@ -83,6 +85,10 @@ local plugins = {
 				bash = { "shfmt", "shellcheck" },
 				zsh = { "shfmt", "shellcheck" },
 				terraform = { "terraform_fmt" },
+				sql = { "sql-formatter" },
+				mysql = { "sql-formatter" },
+				yaml = { "yamlfmt" },
+				yml = { "yamlfmt" },
 			},
 			format_on_save = {
 				lsp_fallback = true,
