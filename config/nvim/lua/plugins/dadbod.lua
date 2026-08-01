90a70ec78c00c62a5c0679472431f041ac9f4d42
donCESAR12345 <sprit152009@gmail.com>
2026-08-01 01:14:05 -0500

    refactor(nvim): configure vim-dadbod-ui queries directory and add connection mapping

--- config/nvim/lua/plugins/dadbod.lua
+++ config/nvim/lua/plugins/dadbod.lua
@@ -1,33 +1,20 @@
 return {
+	-- Database client suite
 	{
 		"tpope/vim-dadbod",
-		lazy = true,
-	},
-	{
-		"kristijanhusak/vim-dadbod-ui",
-		dependencies = { "tpope/vim-dadbod" },
+		dependencies = {
+			"kristijanhusak/vim-dadbod-ui",
+			"kristijanhusak/vim-dadbod-completion",
+		},
 		cmd = { "DBUI", "DBUIToggle", "DBUIAddConnection", "DBUIFindBuffer" },
+		keys = {
+			{ "<leader>du", "<cmd>DBUIToggle<cr>", desc = "Toggle DB UI" },
+			{ "<leader>db", "<cmd>DBUI<cr>", desc = "Open DB connection list" },
+		},
 		init = function()
 			vim.g.db_ui_use_nerd_fonts = 1
 			vim.g.db_ui_show_database_navigation = 1
-		end,
-		keys = {
-			{ "<leader>du", "<cmd>DBUIToggle<cr>", desc = "Toggle Dadbod UI" },
-		},
-	},
-	{
-		"kristijanhusak/vim-dadbod-completion",
-		dependencies = { "tpope/vim-dadbod", "hrsh7th/nvim-cmp" },
-		ft = { "sql", "mysql", "plsql" },
-		config = function()
-			-- Hook Dadbod completion into nvim-cmp
-			require("cmp").setup.filetype({ "sql", "mysql", "plsql" }, {
-				sources = {
-					{ name = "vim-dadbod-completion" },
-					{ name = "nvim_lsp" },
-					{ name = "buffer" },
-				},
-			})
+			vim.g.db_ui_save_location = vim.fn.expand("~/.local/share/db_ui")
 		end,
 	},
 }
